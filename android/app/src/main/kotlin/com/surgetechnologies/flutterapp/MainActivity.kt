a396dfa8349f8c735c91e193444e6daea2425ecf
ephraimphrase <ephraimphrase@gmail.com>
2022-10-10 07:35:18 +0100

    Initial Commit

---
+++ android/app/src/main/kotlin/com/surgetechnologies/flutterapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.surgetechnologies.flutterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/quote_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+class QuoteCard extends StatelessWidget {
+
+  final Quote quote;
+  final VoidCallback delete;
+  QuoteCard({required this.quote, required this.delete});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.red[600],
+              ),
+            ),
+            SizedBox(
+              height: 6.0,
+            ),
+            Text(
+              quote.author,
+              style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.grey[800]
+              ),
+            ),
+            SizedBox(height: 8.0,),
+            ElevatedButton.icon(onPressed: delete, icon: Icon(Icons.delete), label: Text('Delete Quote'))
+          ],
+        ),
+      ),
+    );
+  }
+}
